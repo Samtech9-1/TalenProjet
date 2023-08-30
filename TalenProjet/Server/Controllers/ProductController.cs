bb9f2c14887ea466fc3cf99d9d88f0fcdcfb7330
Sam Tech <sambouba1@hotmail.fr>
2023-08-30 14:50:55 +0200

    Set data on product table

--- TalenProjet/Server/Controllers/ProductController.cs
+++ TalenProjet/Server/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace TalenProjet.Server.Controllers
         };
 
         [HttpGet]
-        public async Task<IActionResult> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
             return Ok(Products);
         }
