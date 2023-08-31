4914d46cf560e7fa50197765974e1b3679a71ab8
Sam Tech <sambouba1@hotmail.fr>
2023-08-31 14:39:14 +0200

    Create user Account Front + validation

---
+++ TalenProjet/Shared/UserRegister.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalenProjet.Shared
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage ="Email invalide"), EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "le mdp doit avoir minimum 3 carractères et maximum 5 carractères"), StringLength(5, MinimumLength = 3)]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage = "Les mots de passes ne sonts pas identiques")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
